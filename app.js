fa932a19a5f612fa07d0c9884e30cd1339fa8b8c
toeytom <toeytom2551@hotmail.com>
2018-09-16 14:55:11 +0700

    dasdda

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const http = require('http').Server(app)
 const io = require('socket.io')(http)
 
 app.get('/',function(req,res){
+    console.log("asdsa");
     res.sendFile(__dirname+"/index.html")
 })
 io.on('connection',function(socket){
