23fac1a6924ae9844494b48aca7087683380e444
toeytom <toeytom2551@hotmail.com>
2018-09-16 14:41:50 +0700

    dasdas

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname+"/index.html")
+})
+io.on('connection',function(socket){
+    console.log("user connect")
+    socket.on('chat',function(msg){
+       io.emit('chat',msg)
+    })
+})
+http.listen(2000,console.log("port open"))
